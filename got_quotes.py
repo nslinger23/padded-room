ec40b1892b2fcfe0cddef43780ece6ca9c75a470
Nick Sindlinger <sindlinger23@gmail.com>
2016-11-06 20:22:45 -0600

    Added first and last name support as well as some additional quotes

--- got_quotes.py
+++ got_quotes.py
@@ -4,14 +4,19 @@ from flask import Flask
 app = Flask(__name__)
 
 class Quotes(object):
-    def __init__(self,name,gender,words):
-        self._name = name
+    def __init__(self,fname,lname,gender,words):
+        self._fname = fname
+        self._lname = lname
         self._gender = gender
         self._words = words
 
     @property
-    def name(self):
-        return self._name
+    def fname(self):
+        return self._fname
+
+    @property
+    def lname(self):
+        return self._lname
 
     @property
     def gender(self):
@@ -22,15 +27,22 @@ class Quotes(object):
         return self._words
 
 male_quotes = [
-    Quotes("Eddard", "male", "THE MAN WHO PASSES THE SENTENCE SHOULD SWING THE SWORD."),
-    Quotes("Eddard", "male", "WINTER IS COMING."),
-    Quotes("Jaime", "male", "THE THINGS I DO FOR LOVE." ),
+    Quotes("Eddard", "Stark", "male", "THE MAN WHO PASSES THE SENTENCE SHOULD SWING THE SWORD"),
+    Quotes("Eddard", "Stark", "male", "WINTER IS COMING."),
+    Quotes("Jaime", "Lannister", "male", "THE THINGS I DO FOR LOVE."),
+    Quotes("Tywin", "Lannister", "male", "IT'S THE FAMILY NAME THAT LIVES ON. IT'S ALL THAT LIVES ON."),
+    Quotes("Jeor", "Mormont", "male", "WHEN DEAD MEN AND WORSE COME HUNTING … YOU THINK IT MATTERS WHO SITS ON THE IRON THRONE?"),
+    Quotes("Tyrion", "Lannister", "male", "THE MAD KING DID AS HE LIKED. HAS YOUR UNCLE JAIME EVER TOLD YOU WHAT HAPPENED TO HIM?"),
 ]
 
 
 female_quotes = [
-    Quotes("Khaleesi", "female", "THE NEXT TIME YOU RAISE A HAND TO ME WILL BE THE LAST TIME YOU HAVE HANDS!"),
-    Quotes("Cersei", "female", "WHEN YOU PLAY THE GAME OF THRONES, YOU WIN OR YOU DIE"),
+    Quotes("Daenerys", "Targaryen", "female", "THE NEXT TIME YOU RAISE A HAND TO ME WILL BE THE LAST TIME YOU HAVE HANDS!"),
+    Quotes("Daenerys", "Targaryen", "female", "TURN US AWAY, AND WE WILL BURN YOU FIRST."),
+    Quotes("Cersei", "Lannister", "female", "WHEN YOU PLAY THE GAME OF THRONES, YOU WIN OR YOU DIE"),
+    Quotes("Catelyn", "Stark", "female", "I PRAYED TO THE GODS 'TAKE HIM AWAY, MAKE HIM DIE"),
+    Quotes("Cersei", "Lannister", "female", "IF YOU EVER CALL ME SISTER AGAIN, I'LL HAVE YOU STRANGLED IN YOUR SLEEP")
+    Quotes("Arya", "Stark", "female", "Nothing isn't better or worse than anything. Nothing is just nothing"),
 
 ]
 
@@ -50,11 +62,21 @@ def get_quote_gender(gender):
         return "Error"
     return random.choice(data).words
 
-@app.route("/api/v1/random/name/<name>")
-def get_quote_by_name(name):
+@app.route("/api/v1/random/fname/<fname>")
+def get_quote_by_fname(fname):
     data = []
     for x in random_quotes:
-        if x.name == name:
+        if x.fname == fname:
+            data.append(x.words)
+    if data == []:
+        return "Error"
+    return random.choice(data)
+
+@app.route("/api/v1/random/lname/<lname>")
+def get_quote_by_lname(lname):
+    data = []
+    for x in random_quotes:
+        if x.lname == lname:
             data.append(x.words)
     if data == []:
         return "Error"
